18c57a93338340548f7399d40ef5a2fca7f044d5
Hilde Aas Nøst <hilde.nost@gmail.com>
2018-09-25 00:21:13 +0200

    Add bridge table

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+let app = new PIXI.Application({
+	width: 256,
+	height: 350
+	}
+);
+
+document.body.appendChild(app.view);
+
+// bridge tables are usually green
+app.renderer.backgroundColor = 0x009900;
